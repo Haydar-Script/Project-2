0ee706712f515837a69dd0e1d8a7d3cc57f90fca
Haydar-Script <124156886+Haydar-Script@users.noreply.github.com>
2024-01-03 23:43:31 +0200

    Update main.dart
    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,18 @@
 import 'package:flutter/material.dart';
+import 'add_player.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int count = 0;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var scaffold = Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "First App",
-            style: TextStyle(
-              fontSize: 32,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome To Flutter",
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Text(
-                "Counter Value: " + count.toString(),
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              count % 2 == 0 ? "Even" : "Odd",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-          print(count);
-        },
-      ),
-    );
-    return scaffold;
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Haydar Project 2',
+        home: AddPlayer());
   }
 }
